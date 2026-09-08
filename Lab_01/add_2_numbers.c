d0c042a0dbe9a0ace0ee3e55deffd6ce100cc8da
Pravi Bairagi <pravibairagi23@gmail.com>
2026-09-08 11:56:37 +0530

    Add program to sum two integers

---
+++ Lab_01/add_2_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main ()
+{
+    int a ;
+    int b;
+
+    printf("enter the value of a :",a);
+    scanf("%d",&a) ; 
+    
+    printf("enter the value of b :",b);
+    scanf("%d",&b) ; 
+    int c = a+b ;
+    printf("the value of c = %d",c);
+    return 0 ;
+    
+
+
+
+
+}
