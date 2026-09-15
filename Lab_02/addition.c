c9ebc4702e8f79c483c288aa38ed0b7ef273cebc
Pravi Bairagi <pravibairagi23@gmail.com>
2026-09-16 00:51:18 +0530

    rename addition.c

---
+++ Lab_02/addition.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main ()
+{
+    int a ;
+    int b;
+
+    printf("enter the value of a :",a);
+    scanf("%d",&a) ; 
+    
+    printf("enter the value of b :",b);
+    scanf("%d",&b) ; 
+
+    int c = a+b ;
+    printf("the value of c = %d",c);
+    return 0 ;
+    }
