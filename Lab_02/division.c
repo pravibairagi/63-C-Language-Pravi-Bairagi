29de398eb14fca0362e1a143253c239b5ea25ab6
Pravi Bairagi <pravibairagi23@gmail.com>
2026-09-17 00:28:53 +0530

    division program in C

---
+++ Lab_02/division.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+    float A ;
+    printf("Enter the value of A : ",A );
+    scanf("%f",&A);
+
+    float B ;
+    printf("Enter the value of B : ",B);
+    scanf("%f",&B);
+
+    float C = A/B ;
+    printf("The division of A and B : %f ", C);
+    return 0 ;
+
+} 
