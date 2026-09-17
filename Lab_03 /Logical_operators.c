70e2aa99fac0381ab0d7ee4f24d210aa1a853e5a
Pravi Bairagi <pravibairagi23@gmail.com>
2026-09-17 21:36:17 +0530

    Logical_operators.c

---
+++ Lab_03 /Logical_operators.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+    int a = 12 ;
+    int b = 23 ;
+    printf("a > b  : %d (Greater than)\n", a > b);
+    printf("a < b  : %d (Less than)\n", a < b);
+    printf("a == b : %d (Equal to)\n", a == b);
+    printf("a != b : %d (Not equal to)\n\n", a != b);
+    return 0 ;
+
+} 
