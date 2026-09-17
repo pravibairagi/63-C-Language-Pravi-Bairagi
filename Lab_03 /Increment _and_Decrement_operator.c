215ee7e08a92bc53870a140903240f92bf1d681b
Pravi Bairagi <pravibairagi23@gmail.com>
2026-09-17 23:45:05 +0530

    Increment and Decrement operator.c

---
+++ Lab_03 /Increment _and_Decrement_operator.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main ()
+{
+    int x = 5 ;
+
+    printf("The Initial Value of x: %d\n ", x );
+    
+    printf("Postfix Increment ( x++ ) : %d\n ", x++ );
+
+    printf("Prefix Increment ( ++x ): %d\n\n", ++x ); 
+
+    printf("Prefix Decrement ( --x ): %d\n\n", --x );   
+
+    printf("Postfix Decrement ( x-- ): %d\n\n", x-- ); 
+    
+
+    return 0 ;
+
+} 
