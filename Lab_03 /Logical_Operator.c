e18f42b1f85dc30868bd50f19a970bb8d5b91eff
Pravi Bairagi <pravibairagi23@gmail.com>
2026-09-17 22:10:08 +0530

    Logical Operator .C

---
+++ Lab_03 /Logical_Operator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+{
+    int a = 12 ;
+    int b = 23 ;
+    printf("(a && b ) : %d (Logical AND)\n", (a && b));
+    printf("(a || b ) : %d (Logical OR)\n", (a|| b));
+    printf("(! b)   : %d (Logical NOT)\n", (! b)) ; 
+    return 0 ;
+
+} 
