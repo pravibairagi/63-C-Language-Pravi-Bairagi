a7d631836865f6905d194045db84f2ab485552b7
Pravi Bairagi <pravibairagi23@gmail.com>
2026-09-24 19:58:40 +0530

    Unsigned_Operator.c

---
+++ Lab_04/Unsigned_Operator.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main ()
+{
+   unsigned short int a ;
+   printf("Enter the of A : ", a );
+   scanf("%u", &a ) ;
+   printf("The unsingned value of A is : %u ", a);
+    return 0 ;
+}
