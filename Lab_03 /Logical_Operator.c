76dd1c001afa4db211870a3f80f6046557bfa43d
Pravi Bairagi <pravibairagi23@gmail.com>
2026-09-26 16:38:29 +0530

    Logical_operators.c

--- Lab_03 /Logical_Operator.c
+++ Lab_03 /Logical_Operator.c
@@ -3,9 +3,9 @@ int main ()
 {
     int a = 12 ;
     int b = 23 ;
-    printf("(a && b ) : %d (Logical AND)\n", (a && b));
-    printf("(a || b ) : %d (Logical OR)\n", (a|| b));
-    printf("(! b)   : %d (Logical NOT)\n", (! b)) ; 
+    printf("Logical AND (a && b ) : %d \n", (a && b));
+    printf("Logical OR (a || b ) : %d \n", (a|| b));
+    printf("Logical NOT (! b)   : %d \n", (! b)) ; 
     return 0 ;
 
 } 
